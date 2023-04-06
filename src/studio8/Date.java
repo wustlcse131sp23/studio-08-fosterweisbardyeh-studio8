2b767015bf58da4c1b4e1a27ecb26db61ff195b0
jacks <jacks@JacksonPC.dhcp.wustl.edu>
2023-04-06 15:41:04 -0500

    Done <3

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,48 @@
 package studio8;
 
-public class Date {
+import java.util.Objects;
 
+public class Date {
+	
+	private static Date d1 = new Date(4, 6, 23, false);
+	private static Date d2 = new Date(4, 6, 23, true);
+	
+	private int month;
+	private int day;
+	private int year;
+	private boolean isHoliday;
+	
+	public Date(int month, int day, int year, boolean isHoliday) {
+		
+		this.month = month;
+		this.year = year;
+		this.day = day;
+		this.isHoliday = isHoliday;
+
+	}
+
+	public String toString() {
+		
+		System.out.println(month + " - " + day + " - " + year);
+		return month + " - " + day + " - " + year;
+		
+	}
 
     public static void main(String[] args) {
+    	
+    	if (d1.equals(d2)) {
+    		
+    		System.out.println("winner");
+    		
+    	}
+    	
+    	else {
+    		
+    		System.out.println("loser");
+    		
+    	}
 
     }
-
 }
+
+	
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -2,6 +2,24 @@ package studio8;
 
 
 public class Time {
+	
+	private int minute;
+	private int hour;
+	boolean before12;
+
+public Time(int minute, int hour, boolean before12) {
+		
+		this.minute = minute;
+		this.hour = hour;
+		this.before12 = before12;
+
+	}
+	
+	public String toString() {
+		
+		return hour + ":" + minute;
+		
+	}
 
 	public static void main(String[] args) {
  
